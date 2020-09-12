328b2308c0da35dda5aa4a8e60e417d48c7537a6
lidonghui-github <1161656983@qq.com>
2020-09-12 13:37:52 +0800

    springboot+Slf4j Spring Boot使用slf4j进行日志记录

---
+++ springboot-slf4j/src/main/java/cn/hd/MySL4jApplication.java
@@ -0,0 +1,11 @@
+package cn.hd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySL4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySL4jApplication.class);
+    }
+}
